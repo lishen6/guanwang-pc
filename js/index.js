5553d0b110b9c0535fce8925b05927c537232a31
larry <15802127603@163.com>
2017-10-31 00:53:36 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -7,14 +7,7 @@ $(function(){
         prevButton:'.swiper-button-prev',
 		nextButton:'.swiper-button-next'
     })
-	var mySwiper2 = new Swiper ('#swiper-container2', {
-		loop: true,
-//  	pagination : '#pagination1',
-//  	paginationClickable :true,
-//      autoplay: 5000,
-        prevButton:'.swiper-button-prev',
-		nextButton:'.swiper-button-next'
-    })
+
     
 	
 
